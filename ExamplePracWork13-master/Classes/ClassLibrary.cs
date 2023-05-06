57f26a48d938bf850c3efbf261792eb699ab5e0f
NEWVirDiG <128370298+NEWVirDiG@users.noreply.github.com>
2023-05-06 11:56:46 +0300

    Add files via upload

---
+++ ExamplePracWork13-master/Classes/ClassLibrary.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplePracWork13.Classes
+{
+    internal class ClassLibrary
+    {
+        // Предметная область: Библиотека.Минимальный список характеристик: 
+        //	номер читательского билета, 
+        //	ФИО читателя,
+        //	адрес,
+        //	телефон, 
+        //	название книги,
+        //	дата выдачи книги читателю,
+        //	дата сдачи книги читателем.
+
+        //поля класса
+        string numberReaderBillet;
+        string fullname;
+        string adress;
+        string phone;
+        string titleBook;
+        DateTime dateIssue;
+        DateTime dateReturn;
+
+        //свойства для доступа к полям
+       public string NumberReaderBillet
+        { get { return numberReaderBillet;} set { numberReaderBillet = value; } }
+        public string FullName
+        { get { return fullname; } set { fullname = value; } }
+        public string Adress
+        { get { return adress; }  set { adress = value; } }
+        public string Phone
+        { get { return phone; } set { phone = value; } }
+        public string TitleBook
+        { get { return titleBook; } set { titleBook = value; } }
+        public DateTime DateIssue
+        { get { return dateIssue; } set { dateIssue = value; } }
+        public DateTime DateReturn
+        { get { return dateReturn; } set { dateReturn = value; } }
+
+        //конструкторы
+        public ClassLibrary() 
+        {
+            numberReaderBillet = string.Empty;
+            fullname = string.Empty;
+            adress = string.Empty;
+            phone= string.Empty;
+            titleBook= string.Empty;
+            dateIssue = DateTime.Now; // сегодняшняя дата
+            dateReturn = DateTime.Now.AddDays(10); // плюс 10 дней
+        }
+        public ClassLibrary(string number, string fname, 
+            string adr, string ph, string title, 
+            DateTime dateIs, DateTime dateRet)
+        {
+            numberReaderBillet = number;
+            fullname = fname;
+            adress = adr;
+            phone = ph;
+            titleBook = title;
+            dateIssue = dateIs;
+            dateReturn = dateRet;
+            
+        }
+    }
+}
---
+++ ExamplePracWork13-master/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ExamplePracWork13.Classes;
+
+namespace ExamplePracWork13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //список объектов класса ClassLibrary
+            
+            List<ClassLibrary> libraries = new List<ClassLibrary>();
+
+            //один объект класса ClassLibrary
+            ClassLibrary library = new ClassLibrary()
+            {
+                NumberReaderBillet = "0000001",
+                FullName = "Абоба Равно Лайк",
+                Adress = "Москва",
+                Phone = "8 800 555 3535",
+                TitleBook = "Разработчик игр абоба",
+                DateIssue = DateTime.Now,
+                DateReturn = DateTime.Now.AddDays(10)
+            };
+            libraries.Add(library);
+
+            //источник данных таблицы - список объектов 
+            DtgListBooks.ItemsSource= libraries;
+        }
+    }
+}
